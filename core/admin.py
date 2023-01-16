96735e0159e4d1274b26453390db139e9368a5ef
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-01-16 12:31:34 -0300

    Criada a model Evento no banco de dados e um superuser para a página admin. Registrada a model Evento na página de admin e associada com model EventoAdmin de admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core import models
+
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+
+
+admin.site.register(models.Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Cria uma tabela com nome core_evento
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação')
+
+    # Para que o nome da tabela seja "evento" e não "core_evento":
+    # class Meta:
+    #   db_table = "evento"
+
+    def __str__(self) -> models.CharField:
+        return self.titulo
