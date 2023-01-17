bee0506a3648bbcb7a4a41c45d15b344a4fc1136
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-01-16 23:28:04 -0300

    Criada página de login e adicionado botão de logout ao header da página. Adicionado a necessidade de autenticação na view lista_eventos. lista_eventos agora somente exibe os eventos do usuário autenticado.

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('evento/<titulo_evento>/', views.get_local),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from . import models
 
 # Create your views here.
@@ -9,14 +12,41 @@ from . import models
 #     return redirect('/agenda/')
 
 
+def login_user(request) -> HttpResponse:
+    return render(request, 'login.html')
+
+
+def logout_user(request) -> HttpResponse:
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request) -> HttpResponse:
+    # Se o tipo do request não for POST, retorna para a página anterior
+    if not request.POST:
+        return redirect('/')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    usuario = authenticate(username=username, password=password)
+    if not usuario:
+        messages.error(request, "Usuário ou senha inválido!")
+        return redirect('/')
+
+    login(request, usuario)
+    return redirect('/')
+
+
+
+
 def get_local(request, titulo_evento: str) -> HttpResponse:
     evento = models.Evento.objects.get(titulo=titulo_evento)
     return HttpResponse(evento.local)
 
 
+@login_required(login_url="/login/")
 def lista_eventos(request) -> HttpResponse:
-    # usuario = request.user
-    # eventos = models.Evento.objects.filter(usuario=usuario)
-    eventos = models.Evento.objects.all()
+    usuario = request.user
+    eventos = models.Evento.objects.filter(usuario=usuario)
     dados = {'eventos': eventos}
     return render(request, 'agenda.html', dados)
