f8bd6d6df1e3c606360f8351ca3a804b7d1e6f6d
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-02-01 15:09:49 -0300

    Criado package rules para guardar as regras de repetição

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
-from core.regras import TipoRepeticao, PARA_SEMPRE
+from rules.constants import TipoRepeticao, PARA_SEMPRE
 
 
 # Create your models here.
